7331e616eee9c3a320055bffa4bc1755c833a957
Marco van Zwetselaar <io@zwets.it>
2016-11-26 23:41:28 +0300

    Clean up layout, optimise awk

--- in-silico-pcr.sh
+++ in-silico-pcr.sh
@@ -16,7 +16,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#  Created on 2016-02-01
+#  Part of https://github.com/zwets/blast-galley
 
 # Defaults
 
@@ -26,28 +26,28 @@ MAX_MIS=2
 
 # Function to emit information to standard error if VERBOSE is set
 emit() {
-	[ -z "$VERBOSE" ] || echo "$(basename "$0"): $*" >&2
+    [ -z "$VERBOSE" ] || echo "$(basename "$0"): $*" >&2
 }
 
 # Function to exit this script with an error message on stderr
 err_exit() {
-	echo "$(basename "$0"): $*" >&2
-	exit 1
+    echo "$(basename "$0"): $*" >&2
+    exit 1
 }
 
 # Function to clean up files if keep is set
 clean_up() {
-	if [ -z "$KEEP" ]; then
-		emit "cleaning up, removing: $@"
-		rm -f "$@"
-	else
-		emit "keeping intermediate file(s): $@"
-	fi
+    if [ -z "$KEEP" ]; then
+        emit "cleaning up, removing: $@"
+        rm -f "$@"
+    else
+        emit "keeping intermediate file(s): $@"
+    fi
 }
 
 # Function to show usage information and exit
 usage_exit() {
-	echo "
+    echo "
 Usage: $(basename $0) [OPTIONS] FILE
 
   Perform an in silico PCR for the primer pair in FILE.
@@ -62,8 +62,8 @@ Usage: $(basename $0) [OPTIONS] FILE
    -d|--database DB   Blast against database DB (default: $BLAST_DB)
    -k|--keep          Keep and reuse intermediate files
    -v|--verbose       Emit progress messages to standard error
-" >&2
-	exit ${1:-1}
+"
+    exit ${1:-1}
 }
 
 # Parse options
@@ -71,38 +71,38 @@ Usage: $(basename $0) [OPTIONS] FILE
 unset VERBOSE KEEP
 
 while [ $# -ne 0 -a "$(expr "$1" : '\(.[0-9]*\).*')" = "-" ]; do
-	case $1 in
-	-m|--mismatches)
-		shift
-		MAX_MIS=$1
-		;;
-	--mismatches=*)
-		MAX_MIS="${1#--mismatches=}"
-		;;
-	-l|--length)
-		shift
-		MAX_LEN=$1
-		;;
-	--length=*)
-		MAX_LEN="${1#--length=}"
-		;;
-	-d|-db|--db)
-		shift
-		BLAST_DB=$1
-		;;
-	-k|--keep)
-		KEEP=1
-		;;
-	-v|--verbose)
-		VERBOSE=1
-		;;
-	-h|--help)
-		usage_exit 0
-		;;
-	*) usage_exit
-		;;
-	esac
-	shift
+    case $1 in
+    -m|--mismatches)
+        shift
+        MAX_MIS=$1
+        ;;
+    --mismatches=*)
+        MAX_MIS="${1#--mismatches=}"
+        ;;
+    -l|--length)
+        shift
+        MAX_LEN=$1
+        ;;
+    --length=*)
+        MAX_LEN="${1#--length=}"
+        ;;
+    -d|-db|--db)
+        shift
+        BLAST_DB=$1
+        ;;
+    -k|--keep)
+        KEEP=1
+        ;;
+    -v|--verbose)
+        VERBOSE=1
+        ;;
+    -h|--help)
+        usage_exit 0
+        ;;
+    *) usage_exit
+        ;;
+    esac
+    shift
 done
 
 # Check the argument
@@ -132,8 +132,8 @@ emit "Extract the reverse primer: $REV_PRIMER"
 
 emit "BLAST the forward primer against database '$BLAST_DB': $FWD_BLAST"
 [ -n "$KEEP" -a -r "$FWD_BLAST" ] || if ! blastn -task blastn-short -db "$BLAST_DB" -query "$FWD_PRIMER" -ungapped -outfmt "$BLAST_OUTFMT" | grep -v '^#' > "$FWD_BLAST"; then
-	clean_up "$FWD_PRIMER" "$REV_PRIMER" "$FWD_BLAST"
-	err_exit "BLAST of forward primer against database $BLAST_DB failed"
+    clean_up "$FWD_PRIMER" "$REV_PRIMER" "$FWD_BLAST"
+    err_exit "BLAST of forward primer against database $BLAST_DB failed"
 fi
 
 clean_up "$FWD_PRIMER"
@@ -143,8 +143,8 @@ emit "Extract the GI list: $FWD_GILIST"
 
 emit "BLAST the reverse primer against the sequences from the forward primer blast"
 [ -n "$KEEP" -a -r "$REV_BLAST" ] || if ! blastn -task blastn-short -db "$BLAST_DB" -query "$REV_PRIMER" -gilist "$FWD_GILIST" -ungapped -outfmt "$BLAST_OUTFMT" | grep -v '^#' > "$REV_BLAST"; then
-	clean_up "$FWD_PRIMER" "$REV_PRIMER" "$FWD_GILIST" "$FWD_BLAST" "$REV_BLAST"
-	err_exit "BLAST of reverse primer against database $BLAST_DB failed"
+    clean_up "$FWD_PRIMER" "$REV_PRIMER" "$FWD_GILIST" "$FWD_BLAST" "$REV_BLAST"
+    err_exit "BLAST of reverse primer against database $BLAST_DB failed"
 fi
 
 clean_up "$FWD_GILIST" "$REV_PRIMER"
@@ -152,30 +152,31 @@ clean_up "$FWD_GILIST" "$REV_PRIMER"
 emit "Join the two BLAST results"
 awk -b -O -F '\t' -v FWD_BLAST_CSV="$FWD_BLAST" -v MAX_LEN=$MAX_LEN -v MAX_MIS=$MAX_MIS '
 BEGIN {
-	OFS=FS					# Tab-separated output
-	while (getline <FWD_BLAST_CSV == 1) {
-		if ($7 + MAX_MIS >= $5) {	# Filter on mismatch limit ($7 is identical count, $5 is query length)
-			fwd[$1][1] = ""		# Tell awk this is a subarray, see awk(1)
-			split($0,fwd[$1])
-		}
-	}
-	close(FWD_BLAST_CSV)
-	print "gi", "seq_id", "length", "strand", "start", "end", "mm_fwd", "mm_rev", "tax_id", "sci_name", "seq_len", "seq_title"
+    OFS=FS                              # Tab-separated output
+    while (getline <FWD_BLAST_CSV == 1) {
+        if ($7 + MAX_MIS >= $5) {	# Filter on mismatch limit ($7 is identical count, $5 is query length)
+            fwd[$1][1] = ""		# Tell awk this is a subarray, see awk(1)
+            split($0,fwd[$1])
+        }
+    }
+    close(FWD_BLAST_CSV)
+    print "gi", "seq_id", "length", "strand", "start", "end", "mm_fwd", "mm_rev", "tax_id", "sci_name", "seq_len", "seq_title"
 }
 # For every line with at most MAX_MIS mismatches
 $7 + MAX_MIS >= $5 {
-	if (fwd[$1][1] == $1 && $2 != fwd[$1][2]) { 	# check that primers found on opposite strands
-		# Length always difference of starting positions; start is lowest on plus, highest on minus (else do not meet)
-		spos = $2 == "minus" ? fwd[$1][3] : $3		# start position is the start on the plus strand
-		epos = $2 == "minus" ? $3 : fwd[$1][3]		# end position is the start on the minus strand
-		len = 1 + epos - spos
-		# Select if length not negative (PCR will not meet), and within length limit
-		if (len > 0 && len <= MAX_LEN) {
-			print $1, $9, len, fwd[$1][2], spos, epos, fwd[$1][5] - fwd[$1][7], $5 - $7, $10, $11, $12, $13
-		}
-	}
+    if (fwd[$1][1] == $1 && $2 != fwd[$1][2]) { 	# check that primers found on opposite strands
+        # Length always difference of starting positions; start is lowest on plus, highest on minus (else do not meet)
+        spos = $2 == "minus" ? fwd[$1][3] : $3		# start position is the start on the plus strand
+        epos = $2 == "minus" ? $3 : fwd[$1][3]		# end position is the start on the minus strand
+        len = 1 + epos - spos
+        # Select if length not negative (PCR will not meet), and within length limit
+        if (len > 0 && len <= MAX_LEN) {
+            print $1, $9, len, fwd[$1][2], spos, epos, fwd[$1][5] - fwd[$1][7], $5 - $7, $10, $11, $12, $13
+        }
+    }
 }
 ' "$REV_BLAST"
 
 clean_up "$FWD_BLAST" "$REV_BLAST"
 
+# vim: sts=4:sw=4:ai:si:et:
